e6fddfc38f205aca27dcd296e96ba9376f5687fa
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2019-10-19 00:39:39 +0200

    settings

---
+++ website/website/settings/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DJANGO_SERVER_TYPE = os.environ.get('DJANGO_SERVER_TYPE', 'prduction')
+
+if DJANGO_SERVER_TYPE == 'production':
+    from .prod import *
+elif DJANGO_SERVER_TYPE == 'development':
+    from .dev import *
+else:
+    raise ImportError(
+        """
+        Could not read server type from environment variable DJANGO_SERVER_TYPE: 
+        value must be one of ["production", "development"]. 
+        """
+    )
--- website/website/settings/dev.py
+++ website/website/settings/dev.py
@@ -10,9 +10,3 @@ SECRET_KEY = '73wy=wy%r^ehl7u)0o+$xy_w@kcmqd!k8@-lmuw8e_uzz0i&e_'
 ALLOWED_HOSTS = ['*'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-
-try:
-    from .local import *
-except ImportError:
-    pass
