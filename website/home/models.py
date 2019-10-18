7b056a6ebe58abbc000b33cdf2ef32a9e5d79f6c
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2019-10-19 01:03:02 +0200

    first home page

--- website/home/models.py
+++ website/home/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from wagtail.admin.edit_handlers import FieldPanel
 
 from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    pass
+    templates = 'home/home_page.html'
+    max_count = 1
+    banner_title = models.CharField(
+        max_length=128,
+        default=''
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel('banner_title')
+    ]
+
+    class Meta:
+        verbose_name = 'Home Page'
