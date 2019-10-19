50b123276087e34015a2df303e9082f77556a9c9
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2019-10-19 19:30:58 +0200

    simple home page

--- website/home/models.py
+++ website/home/models.py
@@ -5,15 +5,21 @@ from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    templates = 'home/home_page.html'
+    template = 'home/home_page.html'
     max_count = 1
+
     banner_title = models.CharField(
         max_length=128,
-        default=''
+        blank=False,
+    )
+    banner_subtitle = models.CharField(
+        max_length=128,
+        blank=True,
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel('banner_title')
+        FieldPanel('banner_title'),
+        FieldPanel('banner_subtitle'),
     ]
 
     class Meta:
