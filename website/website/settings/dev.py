36019a61a69f6b664658eb46465ce5066e51a3b8
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2019-10-19 17:52:04 +0200

    installed debug toolbar

--- website/website/settings/dev.py
+++ website/website/settings/dev.py
@@ -10,3 +10,18 @@ SECRET_KEY = '73wy=wy%r^ehl7u)0o+$xy_w@kcmqd!k8@-lmuw8e_uzz0i&e_'
 ALLOWED_HOSTS = ['*'] 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
