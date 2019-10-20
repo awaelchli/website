4ff4bac41c7c23cc127a03053a3420591d836988
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2019-10-20 17:29:41 +0200

    blog author snippet

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,10 +1,40 @@
 from django.db import models
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
+from wagtail.snippets.models import register_snippet
 
 from streams import blocks
+
+
+@register_snippet
+class BlogAuthor(models.Model):
+
+    first_name = models.CharField(
+        max_length=128,
+    )
+    last_name = models.CharField(
+        max_length=128,
+    )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel('first_name'),
+                FieldPanel('last_name'),
+            ],
+            heading='Details'
+        )
+    ]
+
+    class Meta:
+        verbose_name = 'Blog Author'
+        verbose_name_plural = 'Blog Authors'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 
 class BlogListingPage(Page):
@@ -26,6 +56,13 @@ class BlogDetailPage(Page):
         blank=True,
     )
 
+    author = models.ForeignKey(
+        BlogAuthor,
+        blank=False,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
     image = models.ForeignKey(
         'wagtailimages.Image',
         blank=True,
@@ -44,5 +81,6 @@ class BlogDetailPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
         ImageChooserPanel('image'),
+        SnippetChooserPanel('author'),
         StreamFieldPanel('content'),
     ]
